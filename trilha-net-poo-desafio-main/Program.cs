6aa35351380d584c551da583cb58c250968e5938
Carolina <almeida.carolinabastos@gmail.com>
2022-09-15 04:48:18 -0300

    implementando heranca e os metodos abstratos

---
+++ trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,12 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Teste Smartphone Nokia");
+Nokia nokia = new Nokia(numero: "789456123", modelo:"NK320", imei:"789123", memoria:128);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("Teste Smartphone Iphone");
+Iphone iphone = new Iphone(numero:"123456789", modelo:"XR", imei:"45678", memoria:64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
